7be21f1c1248d7bb794a5c50f00aa1f02cec88e3
Rajan Kumar Sah <rajansah369@gmail.com>
2023-09-06 19:31:02 +0530

    f

---
+++ demodevopsrjn/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi...This is to test CI/CD DevOps.
+        </p>
+        <p>
+          Learn DevOps
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
